fb93e441e1057b077ea6d18f9898d6df7ca7900d
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 17:19:21 +0000

    implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,20 +3,23 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
   
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+  
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
   
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -27,18 +30,17 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
-  
-  def show
+
+  def show 
     @portfolio_item = Portfolio.find(params[:id])
   end
   
@@ -55,9 +57,6 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-    
-  def angular
-    @angular_portfolio_items = Portfolio.angular
-  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get 'angular_items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular_items', to: 'portfolios#angular'
+  
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      post :toggle_status
+    end
+  end
   
   root to: 'pages#home'
 end
